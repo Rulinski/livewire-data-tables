fffe4519db33285417a7916d46ec375cf5b587be
rulinski <vitalrulinski@gmail.com>
2025-11-15 22:19:57 +0100

    feat: add livewire order page

---
+++ app/Livewire/Order/Index/Page.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire\Order\Index;
+
+use Illuminate\View\View;
+use Livewire\Component;
+
+class Page extends Component
+{
+    public function render(): View
+    {
+        return view('livewire.order.index.page');
+    }
+}
