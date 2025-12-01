ba2a758232f6470c8897233b417cb484f03b0d7a
rulinski <vitalrulinski@gmail.com>
2025-12-01 10:39:55 +0100

    feat: add sortable script @alpinejs/sort

--- resources/views/livewire/todo-list.blade.php
+++ resources/views/livewire/todo-list.blade.php
@@ -1,4 +1,6 @@
 <div class="flex flex-col gap-5">
+    <script defer src="https://cdn.jsdelivr.net/npm/@alpinejs/sort@3.x.x/dist/cdn.min.js"></script>
+
     {{-- Add todo --}}
     <form wire:submit="add">
         <div class="flex gap-2">
@@ -7,9 +9,9 @@
     </form>
 
     {{-- Todo list --}}
-    <div class="grid gap-3 min-w-[20rem]">
+    <div class="grid gap-3 min-w-[20rem]" x-sort>
         @foreach($this->todos as $todo)
-            <div class="group p-1.5 bg-white rounded-full shadow shadow-slate-300">
+            <div x-sort:item="{{$todo->id}}" wire:key="{{$todo->id}}" class="group p-1.5 bg-white rounded-full shadow shadow-slate-300">
                 <div class="px-3 py-1 text-sm text-slate-600">{{$todo->name}}</div>
             </div>
         @endforeach
